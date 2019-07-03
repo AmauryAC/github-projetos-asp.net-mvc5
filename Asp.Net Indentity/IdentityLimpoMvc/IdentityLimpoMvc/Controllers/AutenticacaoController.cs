a7fb02237588ee88bdc8113bbf954b035fe48fd3
Amaury AC <amaury.costa@hotmail.com>
2019-07-03 13:10:18 -0300

    Trabalhando com ASP.Net Identity

---
+++ Asp.Net Indentity/IdentityLimpoMvc/IdentityLimpoMvc/Controllers/AutenticacaoController.cs
@@ -0,0 +1,80 @@
+﻿using IdentityLimpoMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityLimpoMvc.Controllers
+{
+    [AllowAnonymous]
+    public class AutenticacaoController : Controller
+    {
+        //private string GetRedirectUrl;
+
+        // GET: Autenticacao
+        [HttpGet]
+        public ActionResult Login(string returnUrl)
+        {
+            var model = new LoginModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // Não faça isso em produção
+            if(model.Login == "amauryac" && model.Senha == "123")
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "Amaury"),
+                    new Claim(ClaimTypes.Email, "amauryac@hotmail.com"),
+                    new Claim(ClaimTypes.Country, "Brasil")
+                }, "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+
+                authManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            // Autenticação falhou
+            ModelState.AddModelError("", "Usuário ou senha inválidos");
+
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if(string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("Index", "Home");
+            }
+
+            return returnUrl;
+        }
+    }
+}
---
+++ Asp.Net Indentity/IdentityLimpoMvc/IdentityLimpoMvc/Models/LoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityLimpoMvc.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [HiddenInput]
+        public string ReturnUrl { get; set; }
+    }
+}
