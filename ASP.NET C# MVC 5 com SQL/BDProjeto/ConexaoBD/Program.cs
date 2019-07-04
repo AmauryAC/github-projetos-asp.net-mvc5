0c681e4c97ba05ffc38d2e55b67f61ef7c1e05ce
Amaury AC <amaury.costa@hotmail.com>
2019-07-04 20:34:35 -0300

    Adaptando estrutura de pastas e adicionando primeiros arquivos ASP.NET com SQL

---
+++ ASP.NET C# MVC 5 com SQL/BDProjeto/ConexaoBD/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var usuario = new Usuario();
+            var usuarioAplicacao = new UsuarioAplicacao();
+
+            // Insert e Update
+            /*Console.Write("Digite o nome do usuário: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o cargo do usuário: ");
+            string cargo = Console.ReadLine();
+
+            DateTime data = DateTime.Now;
+
+            usuario.Nome = nome;
+            usuario.Cargo = cargo;
+            usuario.Data = data;
+            //usuario.Id = 5;
+
+            usuarioAplicacao.Salvar(usuario);*/
+
+            // Delete
+            //usuarioAplicacao.Excluir(7);
+
+            // Select
+            var dados = usuarioAplicacao.ListarTodos();
+
+            foreach(var usuario in dados)
+            {
+                Console.WriteLine("Id: {0}, Nome: {1}, Cargo: {2}, Data: {3}", usuario.Id, usuario.Nome, usuario.Cargo, usuario.Data);
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ ASP.NET C# MVC 5 com SQL/BDProjeto/ConexaoBD/UsuarioAplicacao.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoBD
+{
+    public class UsuarioAplicacao
+    {
+        private BD bd;
+
+        private void Inserir(Usuario usuario)
+        {
+            var strQuery = "";
+
+            strQuery += "INSERT INTO usuarios(nome, cargo, data)";
+            strQuery += String.Format(" VALUES ('{0}', '{1}', '{2}')", usuario.Nome, usuario.Cargo, usuario.Data);
+
+            // Destrói depois de executar
+            using(bd = new BD())
+            {
+                bd.ExecutaComando(strQuery);
+            }
+        }
+
+        private void Alterar(Usuario usuario)
+        {
+            var strQuery = "";
+
+            strQuery += "UPDATE usuarios SET ";
+            strQuery += String.Format("Nome = '{0}',", usuario.Nome);
+            strQuery += String.Format("Cargo = '{0}',", usuario.Cargo);
+            strQuery += String.Format("Data = '{0}' ", usuario.Data);
+            strQuery += String.Format("WHERE usuarioId = '{0}'", usuario.Id);
+
+            using(bd = new BD())
+            {
+                bd.ExecutaComando(strQuery);
+            }
+        }
+
+        public void Salvar(Usuario usuario)
+        {
+            if(usuario.Id > 0)
+            {
+                Alterar(usuario);
+            }
+            else
+            {
+                Inserir(usuario);
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            var strQuery = "";
+
+            strQuery += String.Format("DELETE FROM usuarios WHERE usuarioId = '{0}'", id);
+
+            using(bd = new BD())
+            {
+                bd.ExecutaComando(strQuery);
+            }
+        }
+
+        public List<Usuario> ListarTodos()
+        {
+            var strQuery = "SELECT * FROM usuarios";
+
+            using(bd = new BD())
+            {
+                var retorno = bd.ExecutaComandoComRetorno(strQuery);
+
+                return ReaderEmLista(retorno);
+            }
+        }
+
+        private List<Usuario> ReaderEmLista(SqlDataReader reader)
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+
+            while(reader.Read())
+            {
+                var tempoObjeto = new Usuario()
+                {
+                    Id = int.Parse(reader["usuarioId"].ToString()),
+                    Nome = reader["nome"].ToString(),
+                    Cargo = reader["cargo"].ToString(),
+                    Data = DateTime.Parse(reader["data"].ToString())
+                };
+
+                usuarios.Add(tempoObjeto);
+            }
+
+            return usuarios;
+        }
+    }
+}
